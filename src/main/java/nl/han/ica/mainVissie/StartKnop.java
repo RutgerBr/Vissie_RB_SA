44197e38f555362a5085dffe75f4d3b94edeb95c
san <nafets.a@hotmail.com>
2018-04-05 13:21:21 +0200

    jdoc

--- src/main/java/nl/han/ica/mainVissie/StartKnop.java
+++ src/main/java/nl/han/ica/mainVissie/StartKnop.java
@@ -1,17 +1,31 @@
 package nl.han.ica.mainVissie;
 
 import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
-import nl.han.ica.OOPDProcessingEngineHAN.Sound.Sound;
 import processing.core.PGraphics;
 
+/**
+ * Klasse voor een knop om het spel mee (opnieuw) te starten
+ */
 public class StartKnop extends Knop
 {
 
+    /**
+     * @param g GameEngine
+     * @param o Oceaan
+     * @param x x positie
+     * @param y y positie
+     * @param breedte breedte
+     * @param hoogte hoogte
+     */
     public StartKnop(GameEngine g, Oceaan o, float x, float y, int breedte, int hoogte)
     {
         super(g, o, x, y, breedte, hoogte);
     }
 
+    /**
+     * Tekent de knop op de eerder bepaalde positie
+     * @param pg
+     */
     @Override
     public void tekenKnop(PGraphics pg)
     {
@@ -19,6 +33,9 @@ public class StartKnop extends Knop
         pg.rect(super.x, super.y, super.breedte, super.hoogte);
     }
 
+    /**
+     * Verwijdert alle objecten die met de game te maken hebben, maakt een nieuwe oceaan en speler
+     */
     @Override
     public void doeKnopActie()
     {
