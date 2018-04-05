ce0f457235c16cd262d8f45ad36f30ee3fdcc1fa
san <nafets.a@hotmail.com>
2018-04-05 12:58:52 +0200

    refactor

--- src/main/java/nl/han/ica/mainVissie/mainVissie.java
+++ src/main/java/nl/han/ica/mainVissie/mainVissie.java
@@ -8,20 +8,17 @@ import processing.core.PApplet;
 /**
  * In deze klasse wordt het gehele spel geinitialiseerd
  */
-public class mainVissie extends GameEngine
-{
+public class mainVissie extends GameEngine {
     private Oceaan oceaan;
     private Sound achtergrondGeluid;
     private Menu menu;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         PApplet.main(new String[]{"nl.han.ica.mainVissie.mainVissie"});
     }
 
     @Override
-    public void setupGame()
-    {
+    public void setupGame() {
         maakSoundAan();
         maakOceaanAan();
         oceaan.createViewWithViewport(this, 1000, 800, 1f);
@@ -34,28 +31,23 @@ public class mainVissie extends GameEngine
     /**
      * Maakt de achtergrondmuziek aan en laat deze oneindig door loopen
      */
-    private void maakSoundAan()
-    {
+    private void maakSoundAan() {
         achtergrondGeluid = new Sound(this, "src/main/java/nl/han/ica/mainVissie/media/background.mp3");
         achtergrondGeluid.loop(-1);
     }
 
     @Override
-    public void update() {    }
+    public void update() {
+    }
 
     @Override
-    public void mousePressed() {    }
+    public void mousePressed() {
+    }
 
     /**
      * Maakt een nieuwe oceaan aan
      */
-    private void maakOceaanAan()
-    {
+    private void maakOceaanAan() {
         oceaan = new Oceaan(this);
-    }
-
-    public void pauzeerSpel()
-    {
-        // to-do (could)
     }
 }
